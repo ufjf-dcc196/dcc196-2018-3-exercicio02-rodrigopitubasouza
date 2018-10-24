8772ef0edced4b47061ae41a0a037b0538cec791
Rodrigo Pituba de Souza <rodrigopitubasouza@live.com>
2018-10-24 15:35:01 -0300

    Criação do Adapter

---
+++ app/src/main/java/com/example/rodri/exercicio2_rodrigopitubadesouza/LembreteAdapter.java
@@ -0,0 +1,67 @@
+package com.example.rodri.exercicio2_rodrigopitubadesouza;
+
+
+import android.content.Context;
+import android.database.Cursor;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+
+
+public class LembreteAdapter extends RecyclerView.Adapter<LembreteAdapter.ViewHolder> {
+    private Cursor cursor;
+    public LembreteAdapter(Cursor c){
+        cursor = c;
+    }
+
+    public void setCursor(Cursor c){
+        cursor = c;
+        notifyDataSetChanged();
+
+    }
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        Context context = parent.getContext();
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View livroView = inflater.inflate(R.layout.series_layout,parent,false);
+        ViewHolder holder = new ViewHolder(livroView);
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        int idxTitulo = cursor.getColumnIndexOrThrow(DAO.SeriesContract.Lembrete.COLUMN_NAME_TITULO);
+
+        int idxAutor = cursor.getColumnIndexOrThrow(DAO.SeriesContract.Lembrete.COLUMN_NAME_TEMPORADA);
+
+        int idxEp = cursor.getColumnIndexOrThrow(DAO.SeriesContract.Lembrete.COLUMN_NAME_EP);
+
+        cursor.moveToPosition(position);
+        holder.txtTitulo.setText(cursor.getString(idxTitulo));
+        holder.txtTemporada.setText(cursor.getString(idxAutor));
+        holder.txtEp.setText(String.valueOf(cursor.getString(idxEp)));
+    }
+
+    @Override
+    public int getItemCount() {
+        return cursor.getCount();
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder{
+        public TextView txtTitulo;
+        public TextView txtTemporada;
+        public TextView txtEp;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            txtTitulo = itemView.findViewById(R.id.txt_serie_nome);
+            txtTemporada = itemView.findViewById(R.id.txt_serie_temporada);
+            txtEp = itemView.findViewById(R.id.txt_serie_episodio);
+        }
+    }
+}
